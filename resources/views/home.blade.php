b19f2d71da287dcaf60771c0b60626667de373fb
Santos <tiago.melo@capgemini.com>
2022-07-31 21:52:19 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+
+@section('content')
+    <p>Bem vindos ao E-Diaristas.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
